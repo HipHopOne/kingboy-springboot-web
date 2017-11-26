8156e50eec539242ba6051867a1d571775aaf0d1
KingBoy <kingboyworld@163.com>
2017-11-26 13:21:55 +0800

    init

---
+++ src/main/java/com/kingboy/KingboySpringbootRestApplication.java
@@ -0,0 +1,12 @@
+package com.kingboy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KingboySpringbootRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KingboySpringbootRestApplication.class, args);
+	}
+}
---
+++ src/main/java/com/kingboy/controller/user/UserController.java
@@ -0,0 +1,9 @@
+package com.kingboy.controller.user;
+
+/**
+ * @author kingboy--KingBoyWorld@163.com
+ * @date 2017/11/26 下午1:20
+ * @desc 用户接口服务.
+ */
+public class UserController {
+}
